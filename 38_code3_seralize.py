0459108eab71d98c81ad8936a46feae72a8a2e0c
Nidhishree U R <99992854+Nidhi89717@users.noreply.github.com>
2022-07-02 20:07:37 +0530

    Canonicalizing a tree

---
+++ 38_code3_seralize.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, val = None, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Binarytree:
+    def __init__(self,value):
+        self.root = Node(value)
+
+    def add(self, values_lst, direction_lst):
+        assert len(values_lst) == len(direction_lst)
+        current = self.root
+
+        for i, val in enumerate(values_lst):
+            if direction_lst[i] == 'L':
+                if not current.left:
+                    current.left = Node(values_lst[i])
+                else:
+                    assert current.left.val == values_lst[i]
+                current = current.left
+
+            else:
+                if not current.right:
+                    current.right = Node(values_lst[i])
+                else:
+                    assert current.right.val == values_lst[i]
+                current = current.right
+
+    def parenthesize(self):
+        def _parenthesize(current):
+            lst = [current.val]
+
+            if current.left:
+                lst.append(_parenthesize(current.left))
+            else:
+                lst.append(None)
+
+            if current.right:
+                lst.append(_parenthesize(current.right))
+            else:
+                lst.append(None)
+            
+            return lst
+
+        return _parenthesize(self.root)
+
+if __name__ == '__main__':
+    tree = Binarytree(1)
+    tree.add([12, 4, 7], ['L', 'L', 'L'])
+    tree.add([12, 4, 8], ['L', 'L', 'R'])
+    tree.add([12, 5, 9], ['L', 'R', 'R'])
+    tree.add([3, 6, 10], ['R', 'R', 'L'])
+
+    print(tree.parenthesize())
