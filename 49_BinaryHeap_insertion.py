2cb9408a524bd761f89de8487d7eba8b032d423f
Nidhishree U R <99992854+Nidhi89717@users.noreply.github.com>
2022-07-12 22:18:31 +0530

    Binary Heap Insertion

---
+++ 49_BinaryHeap_insertion.py
@@ -0,0 +1,48 @@
+class Minheap:
+    def __init__(self):
+        self.array = []
+        self.size = 0
+
+    def _left(self,node):
+        p = 2*node + 1
+        return -1 if p > self.size else p
+
+    def _right(self, node):
+        p = 2*node + 2
+        return -1 if p > self.size else p
+
+    def _parent(self, node):
+        return -1 if node == 0 else (node - 1)//2
+
+    def heapify_up(self, child_pos):
+        par_pos = self._parent(child_pos)
+        if child_pos == 0 or self.array[par_pos] < self.array[child_pos]:
+            return
+
+        self.array[child_pos], self.array[par_pos] = \
+            self.array[par_pos], self.array[child_pos]
+        self.heapify_up(par_pos)
+
+    def push(self, key):
+        if self.size + 1 >= len(self.array):
+            self.array.append(None)
+
+        self.array[self.size] = key
+        self.size += 1
+        self.heapify_up(self.size-1)
+
+    def top(self):
+        assert not self.empty()
+        return self.array[0]
+
+    def empty(self):
+        return self.size == 0
+
+if __name__ == '__main__':
+    minheap = Minheap()
+    lst = [2, 17, 22, 10, 8, 37, 14, 19, 7, 6, 5, 12, 25, 30]
+
+    for val in lst:
+        minheap.push(val)
+
+    print(minheap.array)
